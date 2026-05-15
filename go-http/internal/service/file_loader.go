212bbde7dd1f6fa51a847d02c270f73c8214a9ec
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-05-14 21:32:23 -0500

    add modular mock serving

--- go-http/internal/service/file_loader.go
+++ go-http/internal/service/file_loader.go
@@ -3,6 +3,7 @@ package service
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql"
@@ -11,21 +12,30 @@ import (
 
 type FileLoader struct{}
 
-func NewFileLoader() *FileLoader {
-	return &FileLoader{}
-}
-
-func (f *FileLoader) LoadFile(path string, c *graphql.OperationContext) (model.GenericMock, error) {
+func (f *FileLoader) ExtractHeaders(c *graphql.OperationContext) string {
 	mockUserName := c.Headers.Get("mockUserName")
 	if mockUserName == "" {
 		mockUserName = "_default"
 	}
-	data, err := os.ReadFile(fmt.Sprintf("./mock/%s/%s/%s.json", c.Operation.Operation, c.OperationName, mockUserName))
-	if err != nil {
-		errorCode := int32(404)
+	return mockUserName
+}
+
+func (f *FileLoader) handleLoadFileError(err error, c *graphql.OperationContext, mockUserName string) (model.GenericMock, error) {
+	errorCode := int32(404)
+	if mockUserName == "_default" {
 		return model.GenericMock{
 			MockStatusCode: &errorCode,
 		}, err
+	} else {
+		log.Printf("[warning - gql] %s not found, please adda a mock username under './mock/%s/%s/%s.json', attempting to use _default username instead.\n", mockUserName, c.Operation.Operation, c.OperationName, mockUserName)
+		return f.LoadFile(c, "_default")
+	}
+}
+
+func (f *FileLoader) LoadFile(c *graphql.OperationContext, mockUserName string) (model.GenericMock, error) {
+	data, err := os.ReadFile(fmt.Sprintf("./mock/%s/%s/%s.json", c.Operation.Operation, c.OperationName, mockUserName))
+	if err != nil {
+		return f.handleLoadFileError(err, c, mockUserName)
 	}
 
 	var result model.GenericMock
