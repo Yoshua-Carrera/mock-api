e750a65fc658c8378e0efd004d36da60a79737a1
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-05-19 22:04:32 -0500

    config

--- go-http/internal/config/config.go
+++ go-http/internal/config/config.go
@@ -1,1 +1,24 @@
 package config
+
+import "os"
+
+type ServerConfig struct {
+	defaultPort string
+	Port        string
+}
+
+func InitializeConfig() *ServerConfig {
+	return &ServerConfig{
+		defaultPort: "8080",
+	}
+}
+
+func (s *ServerConfig) LoadConfig() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = s.defaultPort
+	}
+
+	s.Port = port
+}
