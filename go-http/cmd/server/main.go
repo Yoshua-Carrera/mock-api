ba85de2e12cd6887c06645d12c6ca12e8defddb3
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-05-20 22:29:03 -0500

    make all the modularzation come together

--- go-http/cmd/server/main.go
+++ go-http/cmd/server/main.go
@@ -1,59 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/handler/extension"
-	"github.com/99designs/gqlgen/graphql/handler/lru"
-	"github.com/99designs/gqlgen/graphql/handler/transport"
-	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/Yoshua-Carrera/mock-api/go-http/internal/graph"
+	"github.com/Yoshua-Carrera/mock-api/go-http/internal/config"
 	"github.com/Yoshua-Carrera/mock-api/go-http/internal/graph/resolvers"
-	"github.com/Yoshua-Carrera/mock-api/go-http/internal/middleware"
+	"github.com/Yoshua-Carrera/mock-api/go-http/internal/http/router"
 	"github.com/Yoshua-Carrera/mock-api/go-http/internal/service"
-	"github.com/vektah/gqlparser/v2/ast"
 )
 
-const defaultPort = "8080"
-
 func main() {
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		port = defaultPort
-	}
-
 	orchestration := service.NewMockOrchestration()
 	resolver := &resolvers.Resolver{
 		Orchestration: orchestration,
 	}
+	c := config.InitializeConfig()
+	c.LoadConfig()
 
-	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: resolver}))
+	r := router.NewRouter(resolver)
 
-	srv.AddTransport(transport.Options{})
-	srv.AddTransport(transport.GET{})
-	srv.AddTransport(transport.POST{})
-
-	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
-
-	srv.Use(extension.Introspection{})
-	srv.Use(extension.AutomaticPersistedQuery{
-		Cache: lru.New[string](100),
-	})
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, world")
-	})
-
-	http.Handle("/gql", playground.Handler("GraphQL sandbox", "/graphql"))
-	http.Handle("/graphql", middleware.Logging(srv))
-
-	log.Printf("[info] - Server starting on port %s", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
-
-	http.ListenAndServe(":8080", nil)
+	log.Printf("[info] - Server starting on port %s", c.Port)
+	log.Fatal(http.ListenAndServe(":"+c.Port, r))
 }
