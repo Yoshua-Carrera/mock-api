4d4f8a59d314df3db53604f354964dfe703aea9b
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-05-21 21:55:16 -0500

    update router to point to abstraction

--- go-http/internal/http/router/router.go
+++ go-http/internal/http/router/router.go
@@ -1,45 +1,21 @@
 package router
 
 import (
-	"fmt"
 	"net/http"
 
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/handler/extension"
-	"github.com/99designs/gqlgen/graphql/handler/lru"
-	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/Yoshua-Carrera/mock-api/go-http/internal/graph"
 	"github.com/Yoshua-Carrera/mock-api/go-http/internal/graph/resolvers"
+	graphUtils "github.com/Yoshua-Carrera/mock-api/go-http/internal/graph/utils"
+	"github.com/Yoshua-Carrera/mock-api/go-http/internal/http/handlers"
 	"github.com/Yoshua-Carrera/mock-api/go-http/internal/middleware"
-	"github.com/vektah/gqlparser/v2/ast"
 )
 
 func NewRouter(resolver *resolvers.Resolver) http.Handler {
 	mux := http.NewServeMux()
-	srv := handler.New(
-		graph.NewExecutableSchema(
-			graph.Config{
-				Resolvers: resolver,
-			},
-		),
-	)
 
-	srv.AddTransport(transport.Options{})
-	srv.AddTransport(transport.POST{})
-	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
-	srv.Use(extension.Introspection{})
-	srv.Use(extension.AutomaticPersistedQuery{
-		Cache: lru.New[string](100),
-	})
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// TODO: Write REST implementation
-		fmt.Fprintln(w, "Hello, world")
-	})
-
+	mux.HandleFunc("/", handlers.HandleMockRequest)
 	mux.Handle("/gql", playground.Handler("GraphQL sandbox", "/graphql"))
-	mux.Handle("/graphql", middleware.Logging(srv))
+	mux.Handle("/graphql", middleware.Logging(graphUtils.BootstrapGraphqlServer(resolver)))
 
 	return mux
 }
