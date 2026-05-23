eca03ba836068020d9c754aa042b801343f18c05
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-05-22 22:27:21 -0500

    rest handler changes

--- go-http/internal/http/handlers/rest.go
+++ go-http/internal/http/handlers/rest.go
@@ -1,10 +1,38 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/Yoshua-Carrera/mock-api/go-http/internal/graph/model"
+	"github.com/Yoshua-Carrera/mock-api/go-http/internal/service"
 )
 
+type genericMock struct {
+	Data  map[string]any `json:"data"`
+	Error []*model.Error `json:"error,omitempty"`
+}
+
 func HandleMockRequest(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, world")
+	w.Header().Set("Content-Type", "application/json")
+	f := service.FileLoader{}
+	path := fmt.Sprintf("%s%s", r.Method, r.URL.Path)
+	mockUserName := f.ExtractHeaders(r.Header)
+	genericMockInternal, path, err := f.LoadFile(path, mockUserName)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(model.Error{
+			Message: "mock not found",
+			Code:    http.StatusNotFound,
+		})
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	time.Sleep(time.Duration(genericMockInternal.MockDelay) * time.Millisecond)
+	json.NewEncoder(w).Encode(genericMock{
+		Data:  genericMockInternal.Data,
+		Error: genericMockInternal.Error,
+	})
 }
