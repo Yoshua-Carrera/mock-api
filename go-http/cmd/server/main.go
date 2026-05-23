5181882014eb1baef8e2345c1c8197e7377efd14
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-05-23 17:18:59 -0500

    provide rest handler struct

--- go-http/cmd/server/main.go
+++ go-http/cmd/server/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Yoshua-Carrera/mock-api/go-http/internal/config"
 	"github.com/Yoshua-Carrera/mock-api/go-http/internal/graph/resolvers"
+	"github.com/Yoshua-Carrera/mock-api/go-http/internal/http/handlers"
 	"github.com/Yoshua-Carrera/mock-api/go-http/internal/http/router"
 	"github.com/Yoshua-Carrera/mock-api/go-http/internal/service"
 )
@@ -15,10 +16,13 @@ func main() {
 	resolver := &resolvers.Resolver{
 		Orchestration: orchestration,
 	}
+	restHandler := &handlers.RestHandler{
+		Orchestration: orchestration,
+	}
 	c := config.InitializeConfig()
 	c.LoadConfig()
 
-	r := router.NewRouter(resolver)
+	r := router.NewRouter(resolver, restHandler)
 
 	log.Printf("[info] - Server starting on port %s", c.Port)
 	log.Fatal(http.ListenAndServe(":"+c.Port, r))
