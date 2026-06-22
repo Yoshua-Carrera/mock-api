e9a4b7b92c4d6af982fc57a2200d5c4e1e79c7ac
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-06-21 20:35:33 -0500

    add support for delay and orchestration

--- elixir-http-mock/lib/elixir_mock_web/controllers/mock_controller.ex
+++ elixir-http-mock/lib/elixir_mock_web/controllers/mock_controller.ex
@@ -1,5 +1,6 @@
 defmodule ElixirMockWeb.MockController do
   use ElixirMockWeb, :controller
+  alias ElixirMockWeb.OrchestrationController, as: OC
 
   @spec readFile(Plug.Conn.t(), String.t()) :: term()
   def readFile(%Plug.Conn{} = conn, mockUserName) do
@@ -10,14 +11,28 @@ defmodule ElixirMockWeb.MockController do
 
   @spec index(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def index(%Plug.Conn{} = conn, %{} = _params) do
+    # Extract username from headers
     mockUserName =
       case Plug.Conn.get_req_header(conn, "mockusername") do
         [header_val | _] -> header_val
         [] -> "_default"
       end
 
+    # Extract read file from mock repository
     f = readFile(conn, mockUserName)
 
-    json(conn, f)
+    # Sleep if mock delay is found
+    case Map.get(f, "mockDelay") do
+      nil -> :ok
+      delay -> Process.sleep(delay)
+    end
+
+    # Extract status code, default to 200 if none is found
+    statusCode = Map.get(f, "mockErrorCode", 200)
+
+    OC.handleOrchestration(f)
+
+    # Return file in the response
+    json(conn |> put_status(statusCode), f |> Map.delete("mockDelay"))
   end
 end
