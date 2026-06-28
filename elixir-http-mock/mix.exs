d0c19c3573f1b4cc26f627e45db3124785ec26ac
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-06-27 22:33:59 -0500

    ping higher version

--- elixir-http-mock/mix.exs
+++ elixir-http-mock/mix.exs
@@ -42,7 +42,7 @@ defmodule ElixirMock.MixProject do
       {:jason, "~> 1.2"},
       {:bandit, "~> 1.5"},
       {:absinthe, "~> 1.7"},
-      {:absinthe_plug, "~> 1.5"}
+      {:absinthe_plug, "~> 1.5.10"}
     ]
   end
 
