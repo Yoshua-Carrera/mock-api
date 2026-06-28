c15dc20d533c455df0a039437f4a6d18b41844f5
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-06-27 22:34:14 -0500

    add schema and register route

--- elixir-http-mock/lib/elixir_mock_web/router.ex
+++ elixir-http-mock/lib/elixir_mock_web/router.ex
@@ -5,6 +5,15 @@ defmodule ElixirMockWeb.Router do
     plug :accepts, ["json"]
   end
 
+  if Mix.env() == :dev do
+    forward "/gql",
+            Absinthe.Plug.GraphiQL,
+            schema: ElixirMockWeb.Schema,
+            interface: :simple
+  end
+
+  forward("/graphql", Absinthe.Plug, schema: ElixirMockWeb.Schema)
+
   scope "/", ElixirMockWeb do
     pipe_through :api
     get "/*path", MockController, :index
---
+++ elixir-http-mock/lib/elixir_mock_web/schema.ex
@@ -0,0 +1,9 @@
+defmodule ElixirMockWeb.Schema do
+  use Absinthe.Schema
+
+  query do
+    field :hello, :string do
+      resolve(fn _, _, _ -> {:ok, "Hello world!"} end)
+    end
+  end
+end
