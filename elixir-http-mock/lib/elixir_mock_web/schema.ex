dfd46c4dfd693b4e375b042d84cf9e3033db7c5a
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-06-28 23:11:48 -0500

    update schema

--- elixir-http-mock/lib/elixir_mock_web/schema.ex
+++ elixir-http-mock/lib/elixir_mock_web/schema.ex
@@ -1,9 +1,16 @@
 defmodule ElixirMockWeb.Schema do
   use Absinthe.Schema
+  alias ElixirMockWeb.Resolvers.MockResolver, as: MC
+
+  object :hello_response do
+    field(:data, :string)
+    field(:operation_type, :string)
+    field(:operation_name, :string)
+  end
 
   query do
-    field :hello, :string do
-      resolve(fn _, _, _ -> {:ok, "Hello world!"} end)
+    field :hello, :hello_response do
+      resolve(fn parent, args, resolution -> MC.hello(parent, args, resolution) end)
     end
   end
 end
