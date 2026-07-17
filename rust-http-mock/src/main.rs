a528765fdcc90b5c789f2802bb7c581b8ec8eda2
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-07-16 22:38:24 -0500

    cleanup

--- rust-http-mock/src/main.rs
+++ rust-http-mock/src/main.rs
@@ -13,17 +13,12 @@ const PORT: u16 = 8080;
 
 #[tokio::main]
 async fn main() {
-    // initialize tracing
     tracing_subscriber::fmt::init();
 
-    // build our application with a route
     let app = Router::new()
-        // `GET /` goes to `root`
         .route("/", get(root))
-        // `POST /users` goes to `create_user`
         .route("/users", post(create_user));
 
-    // run our app with hyper, listening globally on port 8080
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", PORT)).await.unwrap();
 
     info!("Server started, listening on port {}", PORT);
@@ -34,34 +29,26 @@ async fn main() {
 
 }
 
-// basic handler that responds with a static string
 async fn root() -> &'static str {
     "Hello, World!"
 }
 
 async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
     Json(payload): Json<CreateUser>,
 ) -> (StatusCode, Json<User>) {
-    // insert your application logic here
     let user = User {
         id: 1337,
         username: payload.username,
     };
 
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
     (StatusCode::CREATED, Json(user))
 }
 
-// the input to our `create_user` handler
 #[derive(Deserialize)]
 struct CreateUser {
     username: String,
 }
 
-// the output to our `create_user` handler
 #[derive(Serialize)]
 struct User {
     id: u64,
